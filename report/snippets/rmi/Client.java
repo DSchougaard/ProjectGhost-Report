3d7c0ab10a841fbadbd5bc2d1a58d3bcdc1ebcb8
Daniel Schougaard <danielschougaard@gmail.com>
2016-05-27 14:00:18 +0200

    RMI section written

--- report/snippets/rmi/Client.java
+++ report/snippets/rmi/Client.java
@@ -1,14 +1,14 @@
 import java.rmi.*;
 public class Client {
-	public static void main(String args[]) {
-		try {
-			String userServiceURL = "rmi://localhost/UserService";
-			UserService service = (UserService)Naming.lookup(userServiceURL);
+  public static void main(String args[]) {
+    try {
+      String userServiceURL = "rmi://localhost/UserService";
+      UserService service = (UserService)Naming.lookup(userServiceURL);
 
-			System.out.println("Username: " + service.find(1));
-		}
-		catch(Exception e) {
-			System.out.println("Exception: " + e);
-		}
-	}
+      System.out.println("Username: " + service.find(1));
+    }
+    catch(Exception e) {
+      System.out.println("Exception: " + e);
+    }
+  }
 }
--- report/snippets/rmi/User.java
+++
@@ -1,7 +0,0 @@
-public class User{
-    private int id = 0;
-
-    public User(int id){
-        this.id = id;
-    }
-}
